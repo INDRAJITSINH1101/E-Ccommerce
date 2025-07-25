184ac417bf09ff29291ea2fe8643f2d6341103c5
INDRAJITSINH1101 <indrajitsinh234@gmail.com>
2025-07-25 14:11:04 -0700

    Updated files

--- greatkart/greatkart/urls.py
+++ greatkart/greatkart/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('kart.urls')),
     path('store/',include('stor.urls')),
+    path('cart/',include('carts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- greatkart/stor/views.py
+++ greatkart/stor/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,HttpResponse
 from .models import Product
 from kart.models import Category
+from carts.models import Cart,CartItem
+from carts.views import cart_id
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 # Create your views here.
 def stor(request, category_slug=None):
@@ -10,13 +13,19 @@ def stor(request, category_slug=None):
   if category_slug != None:
     categories = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=categories,is_available=True)
+    paginator = Paginator(products, 1)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
     product_count = products.count()
   else:
     products = Product.objects.all().filter(is_available=True)
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
     product_count = products.count()
 
   context = {
-    'products':products,
+    'products':paged_products,
     'product_count':product_count,
   }
   return render(request,'stor/store.html',context)
@@ -24,10 +33,13 @@ def stor(request, category_slug=None):
 def product_detail(request, category_slug,product_slug):
   try:
     single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+    in_cart = CartItem.objects.filter(cart__cart_id=cart_id(request), product=single_product).exists()
+    
   except Exception as e:
     raise e
   
   context = {
-    'single_product':single_product
+    'single_product':single_product,
+    'in_cart':in_cart,
   }
   return render(request,'stor/product_detail.html',context)
